57065390d81eddabf767116f547d9784535754ad
Sivaraj Sekar <ssekar@ctewebapps.com>
2024-07-17 17:41:58 +0530

    commit

--- TNSDC_FinishingSchool.Api/Controllers/MastersController.cs
+++ TNSDC_FinishingSchool.Api/Controllers/MastersController.cs
@@ -37,6 +37,63 @@ namespace TNSDC_FinishingSchool.Api.Controllers
             _response = new APIResponse();
         }
 
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("GetQualification")]
+        public async Task<ActionResult<string>> GetQualification()
+        {
+            string sql = @"EXEC Usp_Masters_Get @inputType, @jsonOutput OUTPUT";
+
+            SqlParameter inputType = new SqlParameter("@inputType", "Qualification");
+            var jsonOutput = new SqlParameter("@jsonOutput", SqlDbType.NVarChar, -1) { Direction = ParameterDirection.Output };
+
+            try
+            {
+                await _dbContext.Database.ExecuteSqlRawAsync(sql, new[] { inputType, jsonOutput });
+                var result = System.Text.Json.JsonSerializer.Deserialize<object>(jsonOutput.Value.ToString());
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.AddError(ex.Message);
+            }
+
+            return Ok(_response);
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("GetMastersIncandidate")]
+        public async Task<ActionResult<string>> GetMastersInCandidate()
+        {
+            string sql = @"EXEC Usp_Masters_Get @inputType, @jsonOutput OUTPUT";
+
+            SqlParameter inputType = new SqlParameter("@inputType", "candidatemasters");
+            var jsonOutput = new SqlParameter("@jsonOutput", SqlDbType.NVarChar, -1) { Direction = ParameterDirection.Output };
+
+            try
+            {
+                await _dbContext.Database.ExecuteSqlRawAsync(sql, new[] { inputType, jsonOutput });
+                var result = System.Text.Json.JsonSerializer.Deserialize<object>(jsonOutput.Value.ToString());
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.AddError(ex.Message);
+            }
+
+            return Ok(_response);
+        }
+
+
         [ProducesResponseType(StatusCodes.Status200OK)]       
         [HttpGet("GetMasterValues")]
         public async Task<ActionResult<string>> GetCertificate(string queryparams)
