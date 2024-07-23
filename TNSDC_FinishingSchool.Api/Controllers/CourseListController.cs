4f6825c65fc8909e59d926b2f4a3e81d2161d9c0
Subashree Venkataramani <svenkataramani@ctewebapps.com>
2024-07-23 12:02:40 +0530

    commit all

--- TNSDC_FinishingSchool.Api/Controllers/CourseListController.cs
+++ TNSDC_FinishingSchool.Api/Controllers/CourseListController.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using System.Data; 
 using System.Net;
 using System.Threading.Tasks;
 using System;
-using TNSDC_FinishingSchool.Bussiness.ApplicationConstants;
+using TNSDC_FinishingSchool.Domain.Contracts;
 using TNSDC_FinishingSchool.Bussiness.Common;
-using TNSDC_FinishingSchool.Bussiness.JWT;
+using TNSDC_FinishingSchool.Bussiness.ApplicationConstants;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using TNSDC_FinishingSchool.Bussiness.Services.Interface;
+using TNSDC_FinishingSchool.Bussiness.DTO.Trainer;
+using System.Linq;
+using TNSDC_FinishingSchool.Bussiness.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http.HttpResults;
 using TNSDC_FinishingSchool.Domain.Models;
+using TNSDC_FinishingSchool.Bussiness.JWT;
+using Microsoft.AspNetCore.Authorization;
+using System.IO;
 
 namespace TNSDC_FinishingSchool.Api.Controllers
 {
@@ -47,7 +61,7 @@ namespace TNSDC_FinishingSchool.Api.Controllers
 
                 string trainerJson = JsonConvert.SerializeObject(courselist);
 
-                string sql = @"EXEC USP_CreateCourse @jsonInput, @jsonOutput OUTPUT";
+                string sql = @"EXEC USP_CreateCourse @jsonInput, @jsonOutput OUTPUT  ";
 
                 var values = new SqlParameter("jsonInput", trainerJson);
                 var jsonOutput = new SqlParameter("jsonOutput", SqlDbType.NVarChar, -1) { Direction = ParameterDirection.Output };
